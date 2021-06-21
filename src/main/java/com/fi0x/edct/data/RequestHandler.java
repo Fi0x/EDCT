250c020ab3165de50039dcd2f041228daa60d81d
Fi0x <felpech@aol.com>
2021-06-21 22:16:35 +0200

    Catching timeout exception for http requests

--- src/main/java/com/fi0x/edct/data/RequestHandler.java
+++ src/main/java/com/fi0x/edct/data/RequestHandler.java
@@ -7,10 +7,7 @@ import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.text.ParseException;
@@ -60,9 +57,16 @@ public class RequestHandler
         {
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            while((inputLine = in.readLine()) != null)
+            try
             {
-                content.append(inputLine);
+                while((inputLine = in.readLine()) != null)
+                {
+                    content.append(inputLine);
+                }
+            }
+            catch(SocketTimeoutException e)
+            {
+                Logger.WARNING("A http request timed out", e);
             }
             in.close();
         } else if(status == 429) Logger.ERROR(429, "Received a 429 status code from a website");
